f42c4bd65a76263fc033cd0ef0a6edb7f4b6da22
sgrazys <s.grazys@gmail.com>
2023-08-13 18:25:04 +0300

    add footer

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,25 @@
+import './Footer.css';
+
+function Footer() {
+	return (
+		<footer>
+			<div class='attribution'>
+				Challenge by{' '}
+				<a
+					href='https://www.frontendmentor.io?ref=challenge'
+					target='_blank'
+					rel='noreferrer'
+				>
+					Frontend Mentor
+				</a>
+				. Coded by{' '}
+				<a href='https://www.linkedin.com/in/saulius-grazys/'>
+					Saulius Grazys
+				</a>
+				.
+			</div>
+		</footer>
+	);
+}
+
+export default Footer;
